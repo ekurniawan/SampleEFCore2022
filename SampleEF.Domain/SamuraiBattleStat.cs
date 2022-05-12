c22b9954aa980ea50ce3425551ec7a854120b84d
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-05-12 11:15:29 +0700

    menambahkan migrasi untuk view

---
+++ SampleEF.Domain/SamuraiBattleStat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEF.Domain
+{
+    /*public class SamuraiBattleStat
+    {
+        public string Name { get; set; }
+        public int? NumberOfBattle { get; set; }
+        public string EarliesBattle { get; set; }
+    }*/
+}
