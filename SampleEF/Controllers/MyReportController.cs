b77265ac62d3ec9d108ab76a528596b12ef0159f
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-05-21 09:46:45 +0700

    mengambil data dari list

--- SampleEF/Controllers/MyReportController.cs
+++ SampleEF/Controllers/MyReportController.cs
@@ -1,16 +1,19 @@
 ﻿using FastReport.Data;
 using FastReport.Web;
 using Microsoft.AspNetCore.Mvc;
+using SampleEF.Data.Dal;
 
 namespace SampleEF.Controllers
 {
     public class MyReportController : Controller
     {
+        private readonly ISamurai _samurai;
         private readonly IWebHostEnvironment _webHostEnvironment;
         private readonly IConfiguration _configuration;
         public MyReportController(IWebHostEnvironment webHostEnvironement,
-            IConfiguration configuration)
+            IConfiguration configuration,ISamurai samurai)
         {
+            _samurai = samurai;
             _webHostEnvironment = webHostEnvironement;
             _configuration = configuration;
         }
@@ -20,16 +23,18 @@ namespace SampleEF.Controllers
             return View();
         }
 
-        public IActionResult GetReportSamurai()
+        public async Task<IActionResult> GetReportSamurai()
         {
+            var samurais = await _samurai.GetAll();
             WebReport web = new WebReport();
             var path = $"{_webHostEnvironment.WebRootPath}\\Reports\\SamuraiReport.frx";
             web.Report.Load(path);
+            web.Report.RegisterData(samurais, "Samurais");
 
-            var mssqlDataConnection = new MsSqlDataConnection();
+            /*var mssqlDataConnection = new MsSqlDataConnection();
             mssqlDataConnection.ConnectionString = _configuration.GetConnectionString("SamuraiConnection");
             var conn = mssqlDataConnection.ConnectionString;
-            web.Report.SetParameterValue("CONN", conn);
+            web.Report.SetParameterValue("CONN", conn);*/
 
             return View(web);
         }
